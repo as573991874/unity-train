afcd627d37fb5e103627175f4d3bdf0291e74950
庄少锟 <zhuangshaokun@zhuangshaokun.local>
2024-03-20 19:02:49 +0800

    <feature> 继续测试镜头

--- Assets/Scripts/Cinemachine/TestCinemachine.cs
+++ Assets/Scripts/Cinemachine/TestCinemachine.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TestCinemachine : MonoBehaviour {
+    public List<CharacterController> playerControllers;
     public CharacterController playerController;
     public float moveSpeed;
     public float rotateSpeed;
@@ -13,27 +14,42 @@ public class TestCinemachine : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        if (Input.GetKeyDown(KeyCode.Alpha0)) {
+            playerController = playerControllers[0];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            playerController = playerControllers[1];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            playerController = playerControllers[2];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            playerController = playerControllers[3];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4)) {
+            playerController = playerControllers[4];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5)) {
+            playerController = playerControllers[5];
+        }
+
+        if (Input.GetKey(KeyCode.Space)) {
+            playerController.transform.localRotation = Quaternion.Euler(0, playerController.transform.localEulerAngles.y, 0);
+            // playerController.transform.position = new Vector3(playerController.transform.position.x, 0, playerController.transform.position.z);
+        }
+
         var h = Input.GetAxis("Horizontal");
         var v = Input.GetAxis("Vertical");
-        var f = playerController.transform.localRotation * new Vector3(h, 0, v);
-        playerController.Move(f * Time.deltaTime * moveSpeed);
+        if (h != 0 || v != 0) {
+            var f = playerController.transform.localRotation * new Vector3(h, 0, v);
+            playerController.Move(f * Time.deltaTime * moveSpeed);
+        }
 
         if (Input.GetKey(KeyCode.Z)) {
             playerController.transform.Rotate(playerController.transform.forward * rotateSpeed * Time.deltaTime, Space.World);
         }
         if (Input.GetKey(KeyCode.C)) {
             playerController.transform.Rotate(-playerController.transform.forward * rotateSpeed * Time.deltaTime, Space.World);
-        }
-
-        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.E)) {
-            if (playerController.transform.localEulerAngles.x > 0.01f) {
-                Quaternion q = Quaternion.Euler(0, playerController.transform.localEulerAngles.y, 0);
-                q = Quaternion.Lerp(playerController.transform.localRotation, q, 0.1f);
-                playerController.transform.localRotation = q;
-                return;
-            } else {
-                playerController.transform.localRotation = Quaternion.Euler(0, playerController.transform.localEulerAngles.y, 0);
-            }
         }
 
         if (Input.GetKey(KeyCode.Q)) {
