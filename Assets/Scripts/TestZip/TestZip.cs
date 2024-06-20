c9b61882710759dc53f27da8dea16bcaa3410111
庄少锟 <zhuangshaokun@zhuangshaokun.local>
2024-06-20 16:22:32 +0800

    <feature> 测试按需解压的效率

--- Assets/Scripts/TestZip/TestZip.cs
+++ Assets/Scripts/TestZip/TestZip.cs
@@ -27,6 +27,15 @@ public class TestZip : MonoBehaviour {
     [Header("多线程下载")]
     public int numberOfThreads;
 
+    [Header("存在的压缩文件")]
+    public string zipFile;
+
+    [Header("解压后的目录")]
+    public string zipExtractPath;
+
+    [Header("读取的文件")]
+    public string readFilePath;
+
     // Update is called once per frame
     void Update() {
 
@@ -48,6 +57,14 @@ public class TestZip : MonoBehaviour {
 
         if (GUI.Button(new Rect((Screen.width - 100) / 2, 180, 100, 50), "DownloadMulti")) {
             DownloadMulti();
+        }
+
+        if (GUI.Button(new Rect((Screen.width - 100) / 2, 240, 100, 50), "ExtractSpeed")) {
+            ExtractSpeed();
+        }
+
+        if (GUI.Button(new Rect((Screen.width - 100) / 2, 300, 100, 50), "ReadFile")) {
+            ReadFileSpeed();
         }
     }
 
@@ -103,5 +120,91 @@ public class TestZip : MonoBehaviour {
             }
         );
         Task.Run(download.Start);
+    }
+
+    void ExtractSpeed() {
+        // 准备文件夹
+        if (Directory.Exists(zipExtractPath)) {
+            Directory.Delete(zipExtractPath, true);
+        }
+        Directory.CreateDirectory(zipExtractPath);
+
+        // 按文件夹解压
+        System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+        stopwatch.Start();
+        ZipFile.ExtractToDirectory(zipFile, zipExtractPath);
+        stopwatch.Stop();
+        Debug.Log($"Extracting by folder took: {stopwatch.Elapsed.TotalSeconds} seconds");
+
+        // 清理文件夹
+        if (Directory.Exists(zipExtractPath)) {
+            Directory.Delete(zipExtractPath, true);
+        }
+        Directory.CreateDirectory(zipExtractPath);
+
+        // 按文件解压
+        stopwatch.Reset();
+        stopwatch.Start();
+        using (ZipArchive archive = ZipFile.OpenRead(zipFile)) {
+            foreach (ZipArchiveEntry entry in archive.Entries) {
+                string destinationPath = Path.GetFullPath(Path.Combine(zipExtractPath, entry.FullName));
+                if (entry.Name == "") {
+                    // 创建目录
+                    Directory.CreateDirectory(destinationPath);
+                } else {
+                    // 创建目录（如果不存在）
+                    Directory.CreateDirectory(Path.GetDirectoryName(destinationPath));
+                    entry.ExtractToFile(destinationPath, true);
+                }
+            }
+        }
+        stopwatch.Stop();
+        Debug.Log($"Extracting by file took: {stopwatch.Elapsed.TotalSeconds} seconds");
+    }
+
+    void ReadFileSpeed() {
+        // 从 zip 文件中读取文件流
+        System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+        stopwatch.Start();
+        ZipArchive archive = ZipFile.OpenRead(zipFile);
+        ZipArchiveEntry entry = archive.GetEntry(readFilePath);
+        if (entry != null) {
+            using (Stream zipStream = entry.Open()) {
+                // 这里可以对 zipStream 进行处理，例如读取内容
+                using (MemoryStream ms = new MemoryStream()) {
+                    zipStream.CopyTo(ms);
+                }
+            }
+        }
+        stopwatch.Stop();
+        Debug.Log($"Reading from zip file took: {stopwatch.Elapsed.TotalMilliseconds} ms");
+
+        // 从缓存的 archive 中读取文件流
+        stopwatch.Reset();
+        stopwatch.Start();
+        entry = archive.GetEntry(readFilePath);
+        if (entry != null) {
+            using (Stream zipStream = entry.Open()) {
+                // 这里可以对 zipStream 进行处理，例如读取内容
+                using (MemoryStream ms = new MemoryStream()) {
+                    zipStream.CopyTo(ms);
+                }
+            }
+        }
+        stopwatch.Stop();
+        Debug.Log($"Reading from zip archive took: {stopwatch.Elapsed.TotalMilliseconds} ms");
+        archive.Dispose();
+
+        // 直接从本地文件读取文件流
+        stopwatch.Reset();
+        stopwatch.Start();
+        using (FileStream localStream = File.OpenRead(Path.Combine(zipExtractPath, readFilePath))) {
+            // 这里可以对 localStream 进行处理，例如读取内容
+            using (MemoryStream ms = new MemoryStream()) {
+                localStream.CopyTo(ms);
+            }
+        }
+        stopwatch.Stop();
+        Debug.Log($"Reading from local file took: {stopwatch.Elapsed.TotalMilliseconds} ms");
     }
 }
